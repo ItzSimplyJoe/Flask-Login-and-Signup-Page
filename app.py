25173f5e98a6ab1b3ec43ab8dddcceb28ffaaa1a
ItzSimplyJoe <joebostock30@gmail.com>
2023-07-19 13:16:34 +0100

    Added Error and Success Messages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from static.database.databases import db
+from static.database.databases import get_db
 
 app = Flask("ItzSimplyJoe")
 app.secret_key = 'superawesomesecretkey1010001'
@@ -9,28 +9,29 @@ app.secret_key = 'superawesomesecretkey1010001'
 def home():
     return render_template("signup.html")
 
+
 @app.route("/login")
 def login():
     return render_template("login.html")
+
 
 @app.route("/signup")
 def signup():
     return render_template("signup.html")
 
-@app.route('/forgotten_password')
-def forgotten_password():
-    return render_template("forgotten_password.html")
 
 @app.route('/success')
 def success():
     return render_template("success.html")
 
+
 @app.route("/createaccount", methods=["POST"])
 def create_account():
     username = request.form["username"]
     password = request.form["password"]
+    db = get_db()  # Get the database connection within the current thread
     others = db.fetch(username)
-    if others != []:
+    if others:
         flash("Username already exists", "error")
         return redirect(url_for("signup"))
     else:
@@ -43,18 +44,20 @@ def create_account():
                 flash("Account created successfully, please login", "success")
                 return redirect(url_for("signup"))
             except:
-                flash("An error occured", "error")
+                flash("An error occurred", "error")
                 return redirect(url_for("signup"))
+
 
 @app.route("/login_account", methods=["POST"])
 def login_account():
     username = request.form["username"]
     password = request.form["password"]
+    db = get_db()  # Get the database connection within the current thread
     user = db.login(username, password)
     if user:
         return redirect(url_for("success"))
     else:
-        flash ("Incorrect username or password", "error")
+        flash("Incorrect username or password", "error")
         return redirect(url_for("login"))
 
 
--- static/database/databases.py
+++ static/database/databases.py
@@ -1,9 +1,20 @@
 import sqlite3
 import hashlib
+from flask import g
+
+DATABASE = 'static/database/database.db'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = Database()
+    return db
+
 
 class Database:
     def __init__(self):
-        self.conn = sqlite3.connect('static/database/database.db')
+        self.conn = sqlite3.connect(DATABASE)
         self.cursor = self.conn.cursor()
         self.cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)')
         self.conn.commit()
@@ -15,13 +26,11 @@ class Database:
     def fetch(self, username):
         self.cursor.execute('SELECT * FROM users WHERE username=?', (username,))
         return self.cursor.fetchall()
-    
+
     def login(self, username, password):
         self.cursor.execute('SELECT * FROM users WHERE username=? AND password=?', (username, self.hashpassword(password)))
         return self.cursor.fetchall()
-    
-    def hashpassword(self,password):
+
+    def hashpassword(self, password):
         password = hashlib.sha256(password.encode()).hexdigest()
         return password
-    
-db = Database()
